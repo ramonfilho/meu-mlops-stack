f39cc9be93b6e571093f51ca2886397423ff0bf7
Ramon Moreira Filho <89781420+ramonfilho@users.noreply.github.com>
2025-07-29 09:34:20 -0300

    adicionado o time stamp no container do chap 2 mod 4

--- chapter_2_model_training/04_vertexai_gpu/model/build_image.sh
+++ chapter_2_model_training/04_vertexai_gpu/model/build_image.sh
@@ -2,6 +2,7 @@
 
 export ARTIFACT_REGISTRY_REPO="model-gpu-vertexai"
 export PROJECT_ID=$(gcloud config get-value project)
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 gcloud artifacts repositories create ${ARTIFACT_REGISTRY_REPO} \
     --repository-format=docker \
@@ -13,7 +14,7 @@ gcloud artifacts repositories create ${ARTIFACT_REGISTRY_REPO} \
 echo "Build Docker image"
 docker build --no-cache -t model_gpu -f chapter_2_model_training/04_vertexai_gpu/model/docker/Dockerfile chapter_2_model_training/04_vertexai_gpu/model
 echo "Tag Docker image"
-docker tag model_gpu us-central1-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/model_gpu:latest
+docker tag model_gpu us-central1-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/model_gpu:${TIMESTAMP}
 echo "Push Docker image"
 gcloud auth configure-docker us-central1-docker.pkg.dev
-docker push us-central1-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/model_gpu:latest
+docker push us-central1-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/model_gpu:${TIMESTAMP}
